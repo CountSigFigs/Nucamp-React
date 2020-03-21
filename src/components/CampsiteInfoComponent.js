d3fb49308f5205139c50040d8cf8631a2a414f2c
Brian Holt <bharrisonholt@gmail.com>
2020-03-21 15:12:30 -0700

    task 3

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
 
 
 
 
-class CampsiteInfo extends Component{
-    renderCampsite (campsite){
+class CampsiteInfo extends Component {
+    renderCampsite(campsite) {
         return (
             <div className="col-md-5 m-1">
                 <Card>
@@ -18,12 +18,35 @@ class CampsiteInfo extends Component{
             </div>
         )
     }
-    render (){
-        if (this.props.campsite){
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comment =>
+                        <div key={comment.id}>
+                            {comment.text}
+                            <br />
+                            --{comment.author}, 
+                            &nbsp;
+                             {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                            <br />
+                            <br />
+                        </div>
+                    )}
+                </div>
+            )
+        }
+        return <div />
+    }
+    render() {
+        if (this.props.campsite) {
             return (
                 <div className="row">
                     {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
                 </div>
+
             )
         }
         return <div />

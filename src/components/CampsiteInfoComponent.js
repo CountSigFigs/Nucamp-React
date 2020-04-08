3c62a86b9d6aa4932344ff6c219cf8b6af7f0bbb
Brian Holt <bharrisonholt@gmail.com>
2020-04-07 21:21:43 -0700

    thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalBody, ModalHeader, Label } from 'reactstrap'
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Loading } from './LoadingComponent'
 
 function RenderCampsite({ campsite }) {
     return (
@@ -159,6 +159,29 @@ class CommentForm extends Component {
 
 
 function CampsiteInfo(props) {
+
+    if (props.isLoading){
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        )
+    }
+
+    if (props.errMess){
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     if (props.campsite) {
         return (
             <div className="container">
